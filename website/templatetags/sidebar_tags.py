8cf1038a5cc37f5d557039540c841d0bb309eb05
Jesus Armando Anaya Orozco <jesus.anaya.dev@gmail.com>
2014-04-06 21:04:55 -0700

    Init new format of sidebars

---
+++ website/templatetags/sidebar_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from django.conf import settings
+from mezzanine.blog.models import BlogPost
+register = template.Library()
+
+
+@register.inclusion_tag("blog/home_blog_posts.html")
+def render_recent_posts():
+    return {
+        'blog_posts': BlogPost.objects.all()[:2],
+        'MEDIA_URL': settings.MEDIA_URL
+    }
