4bc94d03018ae33a0f7746f03231d942350d4950
Jesus Armando Anaya Orozco <jesus.anaya.dev@gmail.com>
2014-04-06 23:36:03 -0700

    Add generic sidebar and blog side bar to respective pages

--- website/templatetags/sidebar_tags.py
+++ website/templatetags/sidebar_tags.py
@@ -7,6 +7,19 @@ register = template.Library()
 @register.inclusion_tag("blog/home_blog_posts.html")
 def render_recent_posts():
     return {
-        'blog_posts': BlogPost.objects.all()[:2],
+        'blog_posts': BlogPost.objects.published()[:2],
         'MEDIA_URL': settings.MEDIA_URL
     }
+
+
+@register.inclusion_tag("sidebars/generic.html")
+def render_generic_sidebar():
+    return {
+        'blog_posts': BlogPost.objects.published()[:3],
+        'MEDIA_URL': settings.MEDIA_URL
+    }
+
+
+@register.inclusion_tag("sidebars/blog.html")
+def render_blog_sidebar():
+    return {}
