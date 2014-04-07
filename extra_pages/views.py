07e4d5b4944285a9f641a7434b56cd86d60368a7
Jesus Armando Anaya Orozco <jesus.anaya.dev@gmail.com>
2014-04-06 22:03:32 -0700

    Update pagination settings in the ListView based pages

--- extra_pages/views.py
+++ extra_pages/views.py
@@ -3,12 +3,14 @@ from .models import TalkPage, CoursePage
 
 
 class Talks(ListView):
-    template_name = "pages/videos.html"
+    template_name = "extra_pages/videos.html"
     queryset = TalkPage.objects.published()
     context_object_name = 'posts'
+    paginate_by = 10
 
 
 class Courses(ListView):
-    template_name = "pages/videos.html"
+    template_name = "extra_pages/videos.html"
     queryset = CoursePage.objects.published()
     context_object_name = 'posts'
+    paginate_by = 10
