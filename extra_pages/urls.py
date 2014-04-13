20faae1f51ea179ba61156011c86a5893fd97137
Jesus Armando Anaya Orozco <jesus.anaya.dev@gmail.com>
2014-04-13 13:51:21 -0700

    Change name to Gallery url

--- extra_pages/urls.py
+++ extra_pages/urls.py
@@ -4,5 +4,5 @@ from .views import Talks, Courses, Galleries
 urlpatterns = patterns("",
     url(r"^talks/$", Talks.as_view(), name="talks"),
     url(r"^courses/$", Courses.as_view(), name="courses"),
-    url(r"^galleries/$", Galleries.as_view(), name="galleries"),
+    url(r"^gallery/$", Galleries.as_view(), name="gallery"),
 )
