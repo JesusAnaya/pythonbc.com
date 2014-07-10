d1bbd686f4351d11c11d1709a920c7a256022e57
Jesus Anaya <jesus.anaya.dev@gmail.com>
2014-07-10 03:41:19 -0700

    Add test to accounts

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import User
+import factory
 
-# Create your tests here.
+
+class UserFactory(factory.DjangoModelFactory):
+    username = "anaya"
+    password = factory.PostGenerationMethodCall('set_password', '1234')
+    photo = factory.django.ImageField()    
+    biography = ""
+    google_plus = ""
+
+    class Meta:
+        model = User
+
+
+class UserTest(TestCase):
+    def test_create_custom_user(self):
+        self.assertIsNotNone(UserFactory().id)
