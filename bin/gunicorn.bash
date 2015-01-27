409fa9458426890c94fed6ba0b8700d30828f157
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-01-27 08:41:04 -0800

    Updated deploy settings

--- bin/gunicorn.bash
+++ bin/gunicorn.bash
@@ -7,9 +7,15 @@ NUM_WORKERS=4
 PORT=9900
 HOST=0.0.0.0
 APP_NAME=config.wsgi
-GUNICORN_PATH=/home/python/pythonbc/venv/bin/gunicorn
+GUNICORN_PATH=/var/lib/jenkins/workspace/pythonbc/venv/bin/gunicorn
 
-cd /home/python/pythonbc
+cd /var/lib/jenkins/workspace/pythonbc
 source ./venv/bin/activate
+
+
+./venv/bin/python manage.py syncdb --settings=config.settings_production
+./venv/bin/python manage.py migrate --all --settings=config.settings_production
+./venv/bin/python manage.py collectstatic --noinput --settings=config.settings_production
+
 
 $GUNICORN_PATH $APP_NAME -b $HOST:$PORT -w $NUM_WORKERS --log-level=debug
