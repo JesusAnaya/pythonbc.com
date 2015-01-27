4b5b3a6532fb5d21ba7e43b2dc7e989949ac1f2d
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-01-27 11:37:45 -0800

    Update bash script to run supervisor

--- bin/gunicorn.bash
+++ bin/gunicorn.bash
@@ -1,12 +1,3 @@
 #!/bin/bash
-set -e
 
-NUM_WORKERS=4
-
-# user/group to run as
-PORT=9900
-HOST=0.0.0.0
-APP_NAME=config.wsgi
-GUNICORN_PATH=/var/lib/jenkins/workspace/pythonbc/venv/bin/gunicorn
-
-$GUNICORN_PATH $APP_NAME -b $HOST:$PORT -w $NUM_WORKERS --log-level=debug
+/home/python/pythonbc/venv/bin/gunicorn config.wsgi -c /home/python/pythonbc/bin/gunicorn.conf.py
