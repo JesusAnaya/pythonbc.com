ebbb73327fd1da1a332650a65c50c12a49052dc6
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-01-27 09:03:06 -0800

    Updated gunicorn bash escript

--- bin/gunicorn.bash
+++ bin/gunicorn.bash
@@ -9,13 +9,4 @@ HOST=0.0.0.0
 APP_NAME=config.wsgi
 GUNICORN_PATH=/var/lib/jenkins/workspace/pythonbc/venv/bin/gunicorn
 
-cd /var/lib/jenkins/workspace/pythonbc
-source ./venv/bin/activate
-
-
-./venv/bin/python manage.py syncdb --settings=config.settings_production
-./venv/bin/python manage.py migrate --all --settings=config.settings_production
-./venv/bin/python manage.py collectstatic --noinput --settings=config.settings_production
-
-
 $GUNICORN_PATH $APP_NAME -b $HOST:$PORT -w $NUM_WORKERS --log-level=debug
