7ef9f70f3c2260dd3f985a9ebe84ca96c4e1a9a5
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-01-27 19:13:11 -0800

    Update bash file.

--- bin/gunicorn.bash
+++ bin/gunicorn.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-HOME_PATH=/home/python/pythonbc
+ROOT_PATH=/home/anaya
+HOME_PATH=$ROOT_PATH/pythonbc
+
+. $ROOT_PATH/.bash_profile
 
 $HOME_PATH/venv/bin/gunicorn config.wsgi -c $HOME_PATH/bin/gunicorn.conf.py
