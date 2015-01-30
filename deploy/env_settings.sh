c0c00b8e6f0e346860aba883bb7878216d5815b9
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-01-29 21:05:24 -0800

    Fixed migration param in github.

--- deploy/env_settings.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export SECRET_KEY="f834-4343-be52-c9230426ac8cd7f0836b-7a23-4bc8-a642-dd5724f754c7527df354-34c8-43ac-856e"
-export NEVERCACHE_KEY="f834-4343-be52-c9230426ac8cd7f0836b-7a23-4bc8-a642-dd5724f754c7527df354-34c8-43ac-856e-gffjgjkhg"
-
