182c773f247c707e201cf66ee56b1d078d127f27
Jesus Anaya <jesus.anaya.dev@gmail.com>
2015-02-04 00:17:35 -0800

    Fixed get blog post at home, changes method all to published.

--- website/templatetags/home_tags.py
+++ website/templatetags/home_tags.py
@@ -7,6 +7,6 @@ register = template.Library()
 @register.inclusion_tag("blog/home_blog_posts.html")
 def render_recent_posts():
     return {
-        'blog_posts': BlogPost.objects.all()[:3],
+        'blog_posts': BlogPost.objects.published()[:3],
         'MEDIA_URL': settings.MEDIA_URL
     }
